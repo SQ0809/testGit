2594235509a210908e91e6d7012f510bf24f30de
SQ <1334143464@qq.com>
2022-06-21 18:47:46 +0800

    third commit

--- src/main/java/com/atsq/git/GitClass.java
+++ src/main/java/com/atsq/git/GitClass.java
@@ -8,5 +8,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git");
+        System.out.println("Hello2 git");
     }
 }
