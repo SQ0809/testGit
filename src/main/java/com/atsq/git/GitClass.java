3daa3adb27eb724991608d7d33f4441c1574904e
SQ <1334143464@qq.com>
2022-06-21 18:59:27 +0800

    hot-fix commit2

--- src/main/java/com/atsq/git/GitClass.java
+++ src/main/java/com/atsq/git/GitClass.java
@@ -10,5 +10,6 @@ public class GitClass {
         System.out.println("Hello1 git");
         System.out.println("Hello2 git");
         System.out.println("Hello3 git");
+        System.out.println("hot-fix test!");
     }
 }
