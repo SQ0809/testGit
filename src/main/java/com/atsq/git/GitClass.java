4f8fca11b99a59d360027e77f1b957e634638180
SQ <1334143464@qq.com>
2022-06-21 18:33:40 +0800

    first commit

---
+++ src/main/java/com/atsq/git/GitClass.java
@@ -0,0 +1,11 @@
+package com.atsq.git;
+
+/**
+ * @author SQ
+ * @create 2022/6/21-0:55
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
